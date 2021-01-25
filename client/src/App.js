2848e0baa099cefadcc4d4c93e5d556a01203efa
Eric Della <ericdella13@gmail.com>
2021-01-24 18:24:48 -0800

    GET request in BubblePage.js to fetch color data

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 import { PrivateRoute } from "./components/PrivateRoute"
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Switch>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/bubblepage">
+          <BubblePage />
+        </PrivateRoute>
+        </Switch>
       </div>
     </Router>
   );
